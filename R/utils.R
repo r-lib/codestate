5899edb386d01f7ddaa80cae3558822a865c382f
Hadley Wickham <h.wickham@gmail.com>
2020-02-13 13:25:26 -0600

    Implement and test local_trace()

--- R/utils.R
+++ R/utils.R
@@ -4,3 +4,12 @@ on_exit <- function(code, env = caller_env()) {
 
   eval_bare(exit, env)
 }
+
+local_trace <- function(fun, tracer, env = caller_env()) {
+  tracer <- enexpr(tracer)
+  fun <- enexpr(fun)
+
+  suppressMessages(trace(fun, tracer, print = FALSE, where = env))
+  on_exit(suppressMessages(untrace(!!fun)), env = env)
+}
+
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -11,3 +11,12 @@ test_that("on_exit runs in caller env", {
   f()
   expect_equal(a, 2)
 })
+
+test_that("can temporarily trace a function", {
+  f <- function() {
+    local_trace(mean, signal("", class = "traced"))
+    mean(1:10)
+  }
+  expect_condition(f(), class = "traced")
+  expect_condition(mean(1:10), NA)
+})
