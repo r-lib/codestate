4c7b277a8223b6ae8dd588164af7797cfa6c2741
Hadley Wickham <h.wickham@gmail.com>
2020-02-16 18:03:11 -0600

    Simplify side_effects a bit

--- R/side-effects.R
+++ R/side-effects.R
@@ -2,32 +2,28 @@ side_effects <- function(chunk) {
   td <- chunk$tracking
   state <- chunk$state
 
+  out <- list()
+
   # Bindings are potentially large, so we want to save in .RDS
-  binding_set <- td$value[td$action == "set" & td$name == "binding"]
-  binding_set <- intersect(binding_set, env_names(chunk$env))
-  bindings <- env_get_list(chunk$env, binding_set)
+  out$bindings <- td$value[td$action == "set" & td$name == "binding"]
+  out$bindings <- intersect(out$bindings, env_names(chunk$env))
 
   if (!identical(state$old$seed, state$new$seed)) {
-    bindings$.Random.seed <- state$new$seed
+    out$seed <- state$new$seed
   }
 
-  # All other side-effects are short values, so we want to record in .yaml
-  effects <- list()
-
-  effects$bindings <- binding_set
-
   options_set <- td$value[td$action == "set" & td$name == "option"]
-  effects$options <- list_diff(state$old$options[options_set], state$new$options[options_set])
+  out$options <- list_diff(state$old$options[options_set], state$new$options[options_set])
 
   if (!identical(state$old$wd, state$new$wd)) {
-    effects$wd <- state$new$wd
+    out$wd <- state$new$wd
   }
 
   if (!identical(state$old$search, state$new$search)) {
     new_packages <- setdiff(state$new$search, state$old$search)
     new_packages <- sub("^package:", "", new_packages)
-    effects$packages <- new_packages
+    out$packages <- new_packages
   }
 
-  list(bindings = bindings, effects = effects)
+  out
 }
--- tests/testthat/test-side-effects.R
+++ tests/testthat/test-side-effects.R
@@ -1,28 +1,31 @@
 test_that("capture key side effects", {
   dir <- normalizePath(tempdir())
   chunk <- track({
-    x <- 1
     old_opt <- options("chonky" = 1)
     old_wd <- setwd(dir)
   })
   options(old_opt)
   setwd(old_wd)
 
-  out <- side_effects(chunk)
-  expect_equal(out$bindings$x, 1)
-  expect_equal(out$effects$options, list(chonky = 1))
-  expect_equal(out$effects$wd, dir)
+  effects <- side_effects(chunk)
+  expect_equal(effects$bindings, c("old_opt", "old_wd"))
+  expect_equal(effects$options, list(chonky = 1))
+  expect_equal(effects$wd, dir)
 })
 
 test_that("records random seed if changed", {
   chunk <- track(runif(10))
-  out <- side_effects(chunk)
-  expect_equal(out$bindings$.Random.seed, .Random.seed)
+  effects <- side_effects(chunk)
+  expect_equal(effects$seed, .Random.seed)
+
+  chunk <- track(1 + 1)
+  effects <- side_effects(chunk)
+  expect_equal(effects$seed, NULL)
 })
 
 test_that("records package attachment", {
   chunk <- track(library("R6"))
   detach("package:R6")
-  out <- side_effects(chunk)
-  expect_equal(out$effects$packages, "R6")
+  effects <- side_effects(chunk)
+  expect_equal(effects$packages, "R6")
 })
