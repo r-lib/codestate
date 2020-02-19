7d70f4157e36afddf9bcdf28bf32364046e9076b
Hadley Wickham <h.wickham@gmail.com>
2020-02-19 14:34:55 -0600

    Basic serialisation code

--- R/process.R
+++ R/process.R
@@ -9,3 +9,15 @@ process <- function(..., .env = caller_env()) {
 chunk_names <- function(x) {
   ifelse(x == "", paste0("chunk", seq_along(x)), x)
 }
+
+chunks_write <- function(chunks, path) {
+  paths <- fs::path(path, names(chunks), ext = "qs")
+  invisible(map2(chunks, paths, qs::qsave))
+}
+
+chunks_read <- function(path) {
+  dir <- fs::dir_ls(path, glob = "*.qs")
+  names <- fs::path_ext_remove(fs::path_file(dir))
+
+  set_names(map(dir, qs::qread), names)
+}
